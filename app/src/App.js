1f8abedb2f8befd24c534eb12f893ae3f83b81b3
stevekutz <stkutz@gmail.com>
2019-12-29 22:08:33 -0600

    added TimesList with select options

--- app/src/App.js
+++ app/src/App.js
@@ -1,18 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
+import TimesList from './components/times-list'
 
-import firebase from './firebase';
 
-firebase.firestore().collection('times').add({
-    title: 'Rubik\'s Cube',
-    time_seconds: 45
-});
+//  => Was added earlier to just test we can connect to database
+// import firebase from './firebase';
+
+// firebase.firestore().collection('times').add({
+//     title: 'Rubik\'s Cube',
+//     time_seconds: 45
+// });
 
 function App() {
   return (
     <div className="App">
         <h1>Just Clock It</h1>
+        <TimesList/>
     </div>
   );
 }
---
+++ app/src/components/times-list.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+const TimesList = () => {
+
+    return (
+        <div>
+            <h2> Times List</h2>
+            <div>
+                <label> Sort By: </label> {' '}
+                <select>
+                    <option> Time [fastes first]</option>
+                    <option> Time [slowest first]</option>
+                    <option disabled > --- </option>
+                    <option> Title [a-z]</option>
+                    <option> Title [z-a]</option>
+                
+                
+                </select>            
+            
+            </div>
+        
+        
+        </div>
+    )
+}
+
+export default TimesList;
